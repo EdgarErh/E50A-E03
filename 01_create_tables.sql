219b80ef666ac6b92f596b3f5ab4891f28d144a3
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 19:22:53 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -23,6 +23,15 @@ CREATE TABLE detalle_pedido (
     id_producto INT REFERENCES productos(id_producto),
     cantidad INT
 );
+
+CREATE TABLE auditoria_pedidos (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_cliente INT,
+    fecha_pedido DATE,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
 -- Clientes
 INSERT INTO clientes (nombre, correo) VALUES
 ('Ana Torres', 'ana@example.com'),
@@ -32,9 +41,10 @@ INSERT INTO clientes (nombre, correo) VALUES
 INSERT INTO productos (nombre, precio) VALUES
 ('Laptop', 1200.00),
 ('Mouse', 25.50),
-('Teclado', 45.00);
+('Teclado', 45.00),
+('Monitor', 300.00);
 
--- Pedidos
+-- Pedidos 
 INSERT INTO pedidos (id_cliente, fecha) VALUES
 (1, '2025-05-01'),
 (2, '2025-05-02');
@@ -44,3 +54,4 @@ INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad) VALUES
 (1, 1, 1),  -- Ana compra 1 Laptop
 (1, 2, 2),  -- Ana compra 2 Mouse
 (2, 3, 1);  -- Luis compra 1 Teclado
+
