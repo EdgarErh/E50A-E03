609547cfddebe3de497813214840aa039cae2dcd
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 19:14:58 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,17 @@
 
+CREATE VIEW vista_detalle_pedidos AS
+SELECT
+    p.id_pedido,
+    c.nombre AS nombre_cliente,
+    pr.nombre AS nombre_producto,
+    dp.cantidad,
+    pr.precio,
+    (dp.cantidad * pr.precio) AS total_linea
+FROM
+    detalle_pedido dp
+JOIN
+    pedidos p ON dp.id_pedido = p.id_pedido
+JOIN
+    clientes c ON p.id_cliente = c.id_cliente
+JOIN
+    productos pr ON dp.id_producto = pr.id_producto;
